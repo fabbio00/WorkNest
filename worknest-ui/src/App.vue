c1577c6377aad3c4d23827f7b77e2812de94509a
anduz15 <riccardo.andena@gmail.com>
2024-03-22 16:10:15 +0100

    setting up docker infrastructure

--- worknest-ui/src/App.vue
+++ worknest-ui/src/App.vue
@@ -8,9 +8,9 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Ciao!" />
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">Hello</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
--- worknest-ui/vite.config.js
+++ worknest-ui/vite.config.js
@@ -8,6 +8,15 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    host: true,
+    port: 8000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
