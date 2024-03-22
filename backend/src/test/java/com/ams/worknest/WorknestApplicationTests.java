70e036a69cf22689b228513df55c5ad54f99b69d
Federico Salvo <federicosalvo@Federicos-MacBook-Air.local>
2024-03-22 15:42:36 +0100

    spring installation

---
+++ backend/src/test/java/com/ams/worknest/WorknestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.worknest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorknestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
