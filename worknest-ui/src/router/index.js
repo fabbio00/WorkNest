698015f9d21264d825d84225031d3d1f1a2a873f
Riccardo Andena <r.andena@campus.unimib.it>
2024-04-06 15:30:47 +0200

    crated frontend structure and implemented frontend to display available workstation

--- worknest-ui/src/router/index.js
+++ worknest-ui/src/router/index.js
@@ -3,7 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
+    {
+      path: '/booking',
+      name: 'booking',
+      component: () => import('../views/BookingWorkStation.vue')
+    },
   ]
 })
 
--- worknest-ui/src/services/api.services.js
+++ worknest-ui/src/services/api.services.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 
 class ApiService{
+
+    find_occupied_desks(date){
+        return axios.get('http://localhost:8080/bookings/findDesks?date=' + date).then((res) => {
+            const desks = res.data.map(resource => resource.workStationId)
+            return desks;
+        });
+    }
     
 }
 
