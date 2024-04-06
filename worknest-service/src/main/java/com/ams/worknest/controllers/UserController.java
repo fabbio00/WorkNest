898a49d70d0f38f8689eece85ac9658af6c6eb16
Federico Salvo <f.salvo2@campus.unimib.it>
2024-04-06 12:26:59 +0200

    created user controller

--- worknest-service/src/main/java/com/ams/worknest/controllers/UserController.java
+++ worknest-service/src/main/java/com/ams/worknest/controllers/UserController.java
@@ -1,9 +1,15 @@
 package com.ams.worknest.controllers;
 
+import com.ams.worknest.model.dto.UserDto;
+import com.ams.worknest.model.resources.UserResource;
+import com.ams.worknest.services.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Slf4j
 @RestController
@@ -11,5 +17,20 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
+
+    @PostMapping()
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResource createUser(@RequestBody UserDto userDto) {
+        log.info("POST | /users | userDto = {}", userDto);
+        return userService.createUser(userDto);
+    }
+
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserResource getUser(@PathVariable("userId") UUID id) {
+        log.info("GET | /users/{}", id);
+        return userService.getUser(id);
+    }
 
 }
