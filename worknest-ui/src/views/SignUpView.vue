d19a83d566c750d2922f37d25001af0894a574b6
Federico Salvo <f.salvo2@campus.unimib.it>
2024-04-07 13:04:56 +0200

    added validation field for sign up and password encryption

--- worknest-ui/src/views/SignUpView.vue
+++ worknest-ui/src/views/SignUpView.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { useVuelidate } from "@vuelidate/core";
+import {
+  required,
+  email,
+  minLength,
+  maxLength,
+  numeric,
+} from "@vuelidate/validators";
+</script>
+
+
 <template>
 
     <v-form @submit.prevent="createUser()">
@@ -35,7 +47,10 @@
                 </v-col>
                 <v-col cols="12" md="6">
                     <div class="text-overline text-large-emphasis">E-mail</div>
-                    <v-text-field v-model="user.email" label="E-mail" variant="outlined" color="indigo"></v-text-field>
+                    <v-text-field v-model="user.email" label="E-mail" variant="outlined" color="indigo"
+                        :error-messages="v$.user.email.$errors.map(e => e.$message)" @blur="v$.user.email.$touch"
+                        @input="v$.user.email.$touch"
+                    ></v-text-field>
                 </v-col>
             </v-row>
             <v-row>
@@ -53,12 +68,22 @@
             <v-row>
                 <v-col cols="12" md="6">
                     <div class="text-overline text-large-emphasis">Password</div>
-                    <v-text-field v-model="user.password" :type="'password'" label="Password" @input="validatePassword"
-                        variant="outlined" color="indigo"></v-text-field>
+                    <v-text-field v-model="user.password" label="Password"
+                        variant="outlined" color="indigo"
+                        :type="passwordVisible ? 'text' : 'password'"
+                        :append-inner-icon="passwordVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                        @click:append-inner="passwordVisible = !passwordVisible"
+                        :error-messages="v$.user.password.$invalid ? 'Password must contain at least one lowercase letter, one uppercase letter, one digit, and be at least 8 characters long' : [] " @blur="v$.user.password.$touch"
+                        @input="v$.user.password.$touch">
+                    </v-text-field>
                 </v-col>
                 <v-col cols="12" md="6">
                     <div class="text-overline text-large-emphasis">Confirm password</div>
-                    <v-text-field v-model="password_confirmed" :type="'password'" label="Confirm Password"
+                    <v-text-field v-model="password_confirmed" label="Confirm Password"
+                    :type="passwordConfirmedVisible ? 'text' : 'password'"
+                    :append-inner-icon="passwordConfirmedVisible ? 'mdi-eye-off' : 'mdi-eye'"
+                    @click:append-inner="passwordConfirmedVisible = !passwordConfirmedVisible"
+                    :error-messages=" !isPasswordValid && v$.password_confirmed.$dirty ? 'Password are different!' : [] " @blur="v$.password_confirmed.$touch"
                         @input="validatePassword" variant="outlined" color="indigo"></v-text-field>
                 </v-col>
                 <v-col cols="12" md="4" class="d-flex justify-left align-self-center">
@@ -74,7 +99,7 @@
             </v-row>
             <v-row>
                 <v-col cols="12" class="d-flex justify-center align-self-center">
-                    <v-btn :disabled="!isPasswordValid" prepend-icon="mdi-check" size="x-large" color="blue-darken-4"
+                    <v-btn :disabled="!isPasswordValid || v$.user.$invalid" prepend-icon="mdi-check" size="x-large" color="blue-darken-4"
                         type="submit">
                         <template v-slot:prepend>
                             <v-icon size="large" color="green-lighten-1"></v-icon>
@@ -87,6 +112,14 @@
 </template>
 
 <script>
+
+import UserServices from "../services/user.services";
+import { helpers } from "@vuelidate/validators";
+
+const checkPassword = helpers.regex(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&,-.#'^"£=(){}%€;:~<>+|/])[A-Za-z\d@$!%*?&,-.#'^"£=(){}%€;:~<>+|/]{8,30}$/
+);
+
 /**
 * User Registration Form Component
 * This component is used for the user registration process, including input fields for user details and password validation.
@@ -117,7 +150,10 @@ export default {
                 barrerFreeFlag: false,
             },
             isPasswordValid: false,
-            password_confirmed: ""
+            password_confirmed: "",
+            passwordVisible: false,
+            passwordConfirmedVisible: false,
+            v$: useVuelidate({ $lazy: true, $autoDirty: true }),
         };
     },
     methods: {
@@ -126,77 +162,25 @@ export default {
         },
         createUser() {
             if (this.isPasswordValid) {
-                console.log(this.user);
-                this.$ApiService.create_user(this.user).then(res => {
-                    console.log(res);
+                let encryptedPassword = UserServices.encryptPassword(this.user.password);
+                let userWithEncryptedPassword = {
+                    ...this.user,
+                    password: encryptedPassword
+                };
+                this.$ApiService.create_user(userWithEncryptedPassword).then(res => {
+                    this.$router.push('/login')
                 });
             } else {
                 alert("Passwords do not match!");
             }
         }
+    },
+    validations: {
+        user: {
+          email: { required, email },
+          password: { required, checkPassword },
+        },
+        password_confirmed: { required },
     }
 }
 </script>
-
-
-<style>
-.main-content {
-    background-color: var(--v-primary-base);
-}
-
-.header-content {
-    color: var(--v-text-primary);
-}
-
-.color-box {
-    width: 200px;
-    height: 50px;
-    margin-bottom: 10px;
-    border: 1px solid #000;
-    padding: 10px;
-    margin-left: auto;
-    margin-right: auto;
-    background-color: var(--v-primary-base);
-    color: #FFF;
-}
-
-.color-box.header {
-    background-color: #3F51B5;
-}
-
-.color-box.navigation {
-    background-color: #3F51B5;
-}
-
-.color-box.footer {
-    background-color: #3F51B5;
-}
-
-.color-box.primary {
-    background-color: #3F51B5;
-}
-
-.color-box.secondary {
-    background-color: #283593;
-}
-
-.color-box.error {
-    background-color: #E53935;
-}
-
-.color-box.warning {
-    background-color: #FDD835;
-}
-
-.color-box.info {
-    background-color: #E3F2FD;
-}
-
-.color-box.success {
-    background-color: #43A047;
-}
-
-.checkbox-centered {
-    margin: auto;
-}
-</style>
