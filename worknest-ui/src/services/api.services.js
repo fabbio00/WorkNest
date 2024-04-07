c68b8e02edd3683f483521295b5b57454e19ca54
Fabio Marini <f.marini14@campus.unimib.it>
2024-04-07 14:51:17 +0200

    added password encryption

--- worknest-ui/src/services/api.services.js
+++ worknest-ui/src/services/api.services.js
@@ -6,11 +6,6 @@ class ApiService{
             email: email,
             password: password
         }).then((res) => {
-            if(res.data && res.data.id){
-                const expirationTime = Date.now() + (3* 3600 * 1000); // 3 ore di durata della sessione
-                localStorage.setItem('expirationTime', expirationTime);
-                localStorage.setItem('userId', res.data.id);
-            }
             return res;
         }).catch((err)=>{
             localStorage.removeItem('userId');
--- worknest-ui/src/views/LoginView.vue
+++ worknest-ui/src/views/LoginView.vue
@@ -9,18 +9,13 @@
   <body class="my-5">
     <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg" @keyup.enter="login()">
       <Transition enter-active-class="animate__animated animate__flipInX">
-      <v-alert
-      v-if="invalidCredentials"
-      border="top"
-      type="warning"
-      class="mb-2"
-    >
-    Invalid email or password</v-alert>
-    </Transition>
+        <v-alert v-if="invalidCredentials" border="top" type="warning" class="mb-2">
+          Invalid email or password</v-alert>
+      </Transition>
       <div class="text-subtitle-1 text-medium-emphasis">Account</div>
 
-      <v-text-field density="compact" placeholder="Email" prepend-inner-icon="mdi-email-outline"
-        variant="outlined" v-model="email" tabindex="1" color="indigo"></v-text-field>
+      <v-text-field density="compact" placeholder="Email" prepend-inner-icon="mdi-email-outline" variant="outlined"
+        v-model="email" tabindex="1" color="indigo"></v-text-field>
 
       <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
         Password
@@ -28,12 +23,13 @@
 
       <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
         density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline" variant="outlined"
-        @click:append-inner="visible = !visible" v-model="password"  tabindex="2" color="indigo"></v-text-field>
+        @click:append-inner="visible = !visible" v-model="password" tabindex="2" color="indigo"></v-text-field>
 
       <v-card class="mb-5" color="surface-variant" variant="tonal">
       </v-card>
 
-      <v-btn class="mb-5" color="blue" size="large" variant="tonal" block @click="login()" :append-icon="'mdi-send-variant'">
+      <v-btn class="mb-5" color="blue" size="large" variant="tonal" block @click="login()"
+        :append-icon="'mdi-send-variant'">
         Log In
       </v-btn>
 
@@ -46,6 +42,7 @@
   </body>
 </template>
 <script>
+import UserServices from "../services/user.services";
 export default {
   data: () => ({
     visible: false,
@@ -54,21 +51,18 @@ export default {
     invalidCredentials: false
   }),
   methods: {
-    login(){
-      this.$ApiService.login(this.email, this.password).then((res)=> {
-       if(res == "unauthorized"){
-        this.invalidCredentials = true;
-       }
-       else{
-        let user = localStorage.getItem('userId');
-        if(user){
+    login() {
+      this.$ApiService.login(this.email, UserServices.encryptPassword(this.password)).then((res) => {
+        if (res == "unauthorized") {
+          this.invalidCredentials = true;
+        }
+        else if (res.data && res.data.id) {
+          const expirationTime = Date.now() + (3 * 3600 * 1000); // 3 ore di durata della sessione
+          localStorage.setItem('expirationTime', expirationTime);
+          localStorage.setItem('userId', res.data.id);
           this.invalidCredentials = false;
           this.$router.push("/");
         }
-        else{
-          this.invalidCredentials = true;
-        }
-       }
       });
     }
   }
